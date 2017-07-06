a64d6d777af0afb72d0d51ed930d3187037989f1
JanMeckelholt <Jan_Meckelholt@web.de>
2017-07-06 19:13:18 +0200

    devise functionality implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-
-
-resources :posts do
-  resources :comments
-end
-
-root to: "posts#index"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+    devise_for :users
+  resources :posts do
+    resources :comments
+  end
 end
